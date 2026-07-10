fa50f5fae835a35372c0c0f19a2f09fe78477d39
weifeng <157676363@qq.com>
2026-07-10 12:56:42 +0800

    fix: add SetSelinuxEnforce=5 to ksud FeatureId enum

--- userspace/ksud/src/feature.rs
+++ userspace/ksud/src/feature.rs
@@ -21,6 +21,7 @@ pub enum FeatureId {
     Sulog = 2,
     AdbRoot = 3,
     SelinuxHide = 4,
+    SetSelinuxEnforce = 5,
     AvcSpoof = 10003,
 }
 
@@ -32,6 +33,7 @@ impl FeatureId {
             2 => Some(Self::Sulog),
             3 => Some(Self::AdbRoot),
             4 => Some(Self::SelinuxHide),
+            5 => Some(Self::SetSelinuxEnforce),
             10003 => Some(Self::AvcSpoof),
             _ => None,
         }
@@ -44,6 +46,7 @@ impl FeatureId {
             Self::Sulog => "sulog",
             Self::AdbRoot => "adb_root",
             Self::SelinuxHide => "selinux_hide",
+            Self::SetSelinuxEnforce => "set_selinux_enforce",
             Self::AvcSpoof => "avc_spoof",
         }
     }
@@ -63,6 +66,9 @@ impl FeatureId {
             Self::SelinuxHide => {
                 "SELinux Hide - sanitize /sys/fs/selinux access results for app UIDs"
             }
+            Self::SetSelinuxEnforce => {
+                "SELinux Enforce - toggle SELinux enforcing mode (requires CONFIG_SECURITY_SELINUX_DEVELOP)"
+            }
             Self::AvcSpoof => {
                 "AVC Spoof - fix selinux context leak due to avc denial"
             }
@@ -77,6 +83,7 @@ fn parse_feature_id(name: &str) -> Result<FeatureId> {
         "sulog" | "2" | "enhanced_security" => Ok(FeatureId::Sulog),
         "adb_root" | "3" => Ok(FeatureId::AdbRoot),
         "selinux_hide" | "4" => Ok(FeatureId::SelinuxHide),
+        "set_selinux_enforce" | "5" => Ok(FeatureId::SetSelinuxEnforce),
         "avc_spoof" | "10003" => Ok(FeatureId::AvcSpoof),
         _ => bail!("Unknown feature: {name}"),
     }
@@ -324,6 +331,7 @@ pub fn list_features() {
         FeatureId::Sulog,
         FeatureId::AdbRoot,
         FeatureId::SelinuxHide,
+        FeatureId::SetSelinuxEnforce,
         FeatureId::AvcSpoof,
     ];
 
@@ -388,6 +396,7 @@ pub fn save_config() -> Result<()> {
         FeatureId::Sulog,
         FeatureId::AdbRoot,
         FeatureId::SelinuxHide,
+        FeatureId::SetSelinuxEnforce,
         FeatureId::AvcSpoof,
     ];
 
